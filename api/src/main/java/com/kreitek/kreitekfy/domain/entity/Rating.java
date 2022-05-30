5638a7147a119a31d1c4deeed3df0ef05502fd29
luisgb98 <luis.guisadobarrera@gmail.com>
2022-05-30 09:42:42 +0200

    UML and Endpoints updated

---
+++ api/src/main/java/com/kreitek/kreitekfy/domain/entity/Rating.java
@@ -0,0 +1,9 @@
+package com.kreitek.kreitekfy.domain.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ratings")
+public class Rating {
+}
---
+++ api/src/main/java/com/kreitek/kreitekfy/domain/entity/Reproduction.java
@@ -0,0 +1,9 @@
+package com.kreitek.kreitekfy.domain.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "reproductions")
+public class Reproduction {
+}
