4f99ef819a15bde02a42c23178dc189e27af987b
Mario Medina <maralfonso.medina@gmail.com>
2022-05-31 10:08:58 +0200

    Deleted modules

--- site/src/app/app-routing.module.ts
+++ site/src/app/app-routing.module.ts
@@ -10,8 +10,6 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'backoffice', loadChildren: () => import('./backoffice/backoffice.module').then(m => m.BackofficeModule) },
-  { path: 'songsheet', loadChildren: () => import('./songsheet/songsheet.module').then(m => m.SongsheetModule) },
   { path: 'genre', component: GenreListComponent },
   { path: 'genre/:genreId', component: GenreFormComponent },
   { path: 'artist', component: ArtistListComponent },
